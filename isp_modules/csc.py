a3111dff7e2396ce017fd16d89c5365ea628ab47
Qiu Jueqin <jqx1991@gmail.com>
2021-10-23 23:14:17 +0800

    remove RGB2YCbCr matrix from configuration

--- isp_modules/csc.py
+++ isp_modules/csc.py
@@ -13,14 +13,15 @@ class CSC(BasicModule):
     def __init__(self, cfg):
         super().__init__(cfg)
 
-        csc = np.array(self.params.csc, dtype=np.int32).T  # x1024, (4, 3) right-matrix
-        self.matrix = csc[:3, :]  # (3, 3) right-matrix
-        self.bias = csc[3, :].reshape(1, 1, 3)  # (1, 1, 3)
+        self.matrix = np.array([[66, 129, 25],
+                                [-38, -74, 112],
+                                [112, -94, -18]], dtype=np.int32).T  # x256
+        self.bias = np.array([16, 128, 128], dtype=np.int32).reshape(1, 1, 3)
 
     def execute(self, data):
         rgb_image = data['rgb_image'].astype(np.int32)
 
-        ycrcb_image = np.right_shift(rgb_image @ self.matrix + self.bias, 10).astype(np.uint8)
+        ycrcb_image = (np.right_shift(rgb_image @ self.matrix, 8) + self.bias).astype(np.uint8)
 
         data['y_image'] = ycrcb_image[..., 0]
         data['cbcr_image'] = ycrcb_image[..., 1:]
