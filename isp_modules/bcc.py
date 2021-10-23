68cd92c322e1f6f495ca2e6f05bbf907d89729c1
Qiu Jueqin <jqx1991@gmail.com>
2021-10-23 23:15:04 +0800

    update BCC algorithm

--- isp_modules/bcc.py
+++ isp_modules/bcc.py
@@ -22,8 +22,8 @@ class BCC(BasicModule):
 
         bcc_y_image = np.clip(y_image + self.brightness_offset, 0, self.cfg.saturation_values.sdr)
 
-        avg_y = bcc_y_image.mean().astype(np.int32)
-        bcc_y_image = np.right_shift((bcc_y_image - avg_y) * self.contrast_gain, 8) + avg_y
+        y_median = np.median(bcc_y_image).astype(np.int32)
+        bcc_y_image = np.right_shift((bcc_y_image - y_median) * self.contrast_gain, 8) + y_median
         bcc_y_image = np.clip(bcc_y_image, 0, self.cfg.saturation_values.sdr)
 
         data['y_image'] = bcc_y_image.astype(np.uint8)
